78de2cd41abaaeeb501f20472c4a6cf862bdd285
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2023-12-19 10:54:00 -0500

    update v0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='cansii',
-    version='0.1',
+    version='0.2',
     packages=['cansii'],
     description='A Python library for terminal text colorization using ANSI escape codes.',
     author='Jakepys Perdomo',
     author_email='perdomojuan187@gmail.com',
     url='https://github.com/JuanPerdomo00/CAnsii',
+    long_description=open('./README.md').read(),
+    long_description_content_type='text/markdown',
     license='GPL-3.0',
     classifiers=[
         'Development Status :: 3 - Alpha',
